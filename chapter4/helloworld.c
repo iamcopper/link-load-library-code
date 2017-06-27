9d37edae78035539bc4c5803a417cf9294c244e8
kang.pan <kang.pan@advantech.com.cn>
2017-06-27 22:22:46 +0800

    Add chapter4-helloworld.c

---
+++ chapter4/helloworld.c
@@ -0,0 +1,34 @@
+/**
+ * tiny hello world.
+ *
+ * Build:
+ * gcc -c -fno-builtin helloworld.c
+ * ld -static -e nomain -o helloworld helloworld.o
+ * 
+ * ld -static -T helloworld.lds -o helloworld helloworld.o
+ */
+
+char *str = "Hello world!\n";
+
+void print()
+{
+	asm("movl $13, %%edx \n\t"
+		"movq %0, %%rcx  \n\t"
+		"movl $0, %%ebx  \n\t"
+		"movl $4, %%eax  \n\t"
+		"int $0x80       \n\t"
+		::"r"(str):"edx", "ecx", "ebx");
+}
+
+void exit()
+{
+	asm("movl $42, %ebx \n\t"
+		"movl $1,  %eax \n\t"
+		"int $0x80      \n\t");
+}
+
+void nomain()
+{
+	print();
+	exit();
+}
